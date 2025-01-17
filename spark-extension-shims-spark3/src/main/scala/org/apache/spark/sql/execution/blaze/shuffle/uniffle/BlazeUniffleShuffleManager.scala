f546d159db7fbb70d783da417e8deb35738451a3
Junfan Zhang <zuston@apache.org>
2025-01-17 22:48:01 +0800

    2/N

--- spark-extension-shims-spark3/src/main/scala/org/apache/spark/sql/execution/blaze/shuffle/uniffle/BlazeUniffleShuffleManager.scala
+++ spark-extension-shims-spark3/src/main/scala/org/apache/spark/sql/execution/blaze/shuffle/uniffle/BlazeUniffleShuffleManager.scala
@@ -1,13 +1,31 @@
+/*
+ * Copyright 2022 The Blaze Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.spark.sql.execution.blaze.shuffle.uniffle
 
+import org.apache.spark.shuffle.writer.RssShuffleWriter
 import org.apache.spark.shuffle.{RssShuffleManager, ShuffleBlockResolver, ShuffleHandle, ShuffleReadMetricsReporter, ShuffleReader, ShuffleWriteMetricsReporter, ShuffleWriter}
 import org.apache.spark.{ShuffleDependency, SparkConf, TaskContext}
 import org.apache.spark.sql.execution.blaze.shuffle.{BlazeRssShuffleManagerBase, BlazeRssShuffleReaderBase, BlazeRssShuffleWriterBase}
 
 class BlazeUniffleShuffleManager(conf: SparkConf, isDriver: Boolean)
-  extends BlazeRssShuffleManagerBase(conf) {
+    extends BlazeRssShuffleManagerBase(conf) {
   private val uniffleShuffleManager: RssShuffleManager = new RssShuffleManager(conf, isDriver);
-  override def registerShuffle[K, V, C](shuffleId: Int, dependency: ShuffleDependency[K, V, C]): ShuffleHandle = {
+  override def registerShuffle[K, V, C](
+      shuffleId: Int,
+      dependency: ShuffleDependency[K, V, C]): ShuffleHandle = {
     uniffleShuffleManager.registerShuffle(shuffleId, dependency)
   }
 
@@ -15,15 +33,39 @@ class BlazeUniffleShuffleManager(conf: SparkConf, isDriver: Boolean)
     uniffleShuffleManager.unregisterShuffle(shuffleId)
   }
 
-  override def getBlazeRssShuffleReader[K, C](handle: ShuffleHandle, startPartition: Int, endPartition: Int, context: TaskContext, metrics: ShuffleReadMetricsReporter): BlazeRssShuffleReaderBase[K, C] = ???
+  override def getBlazeRssShuffleReader[K, C](
+      handle: ShuffleHandle,
+      startPartition: Int,
+      endPartition: Int,
+      context: TaskContext,
+      metrics: ShuffleReadMetricsReporter): BlazeRssShuffleReaderBase[K, C] = ???
 
-  override def getBlazeRssShuffleReader[K, C](handle: ShuffleHandle, startMapIndex: Int, endMapIndex: Int, startPartition: Int, endPartition: Int, context: TaskContext, metrics: ShuffleReadMetricsReporter): BlazeRssShuffleReaderBase[K, C] = ???
+  override def getBlazeRssShuffleReader[K, C](
+      handle: ShuffleHandle,
+      startMapIndex: Int,
+      endMapIndex: Int,
+      startPartition: Int,
+      endPartition: Int,
+      context: TaskContext,
+      metrics: ShuffleReadMetricsReporter): BlazeRssShuffleReaderBase[K, C] = ???
 
-  override def getRssShuffleReader[K, C](handle: ShuffleHandle, startPartition: Int, endPartition: Int, context: TaskContext, metrics: ShuffleReadMetricsReporter): ShuffleReader[K, C] = {
+  override def getRssShuffleReader[K, C](
+      handle: ShuffleHandle,
+      startPartition: Int,
+      endPartition: Int,
+      context: TaskContext,
+      metrics: ShuffleReadMetricsReporter): ShuffleReader[K, C] = {
     uniffleShuffleManager.getReader(handle, startPartition, endPartition, context, metrics)
   }
 
-  override def getRssShuffleReader[K, C](handle: ShuffleHandle, startMapIndex: Int, endMapIndex: Int, startPartition: Int, endPartition: Int, context: TaskContext, metrics: ShuffleReadMetricsReporter): ShuffleReader[K, C] = {
+  override def getRssShuffleReader[K, C](
+      handle: ShuffleHandle,
+      startMapIndex: Int,
+      endMapIndex: Int,
+      startPartition: Int,
+      endPartition: Int,
+      context: TaskContext,
+      metrics: ShuffleReadMetricsReporter): ShuffleReader[K, C] = {
     uniffleShuffleManager.getReaderForRange(
       handle,
       startMapIndex,
@@ -34,11 +76,21 @@ class BlazeUniffleShuffleManager(conf: SparkConf, isDriver: Boolean)
       metrics)
   }
 
-  override def getBlazeRssShuffleWriter[K, V](handle: ShuffleHandle, mapId: Long, context: TaskContext, metrics: ShuffleWriteMetricsReporter): BlazeRssShuffleWriterBase[K, V] = {
-
+  override def getBlazeRssShuffleWriter[K, V](
+      handle: ShuffleHandle,
+      mapId: Long,
+      context: TaskContext,
+      metrics: ShuffleWriteMetricsReporter): BlazeRssShuffleWriterBase[K, V] = {
+    val writer: ShuffleWriter[K, V] =
+      uniffleShuffleManager.getWriter(handle, mapId, context, metrics)
+    new BlazeUniffleShuffleWriter(writer.asInstanceOf[RssShuffleWriter[K, V, _]], metrics)
   }
 
-  override def getRssShuffleWriter[K, V](handle: ShuffleHandle, mapId: Long, context: TaskContext, metrics: ShuffleWriteMetricsReporter): ShuffleWriter[K, V] = {
+  override def getRssShuffleWriter[K, V](
+      handle: ShuffleHandle,
+      mapId: Long,
+      context: TaskContext,
+      metrics: ShuffleWriteMetricsReporter): ShuffleWriter[K, V] = {
     uniffleShuffleManager.getWriter(handle, mapId, context, metrics)
   }
 
--- spark-extension-shims-spark3/src/main/scala/org/apache/spark/sql/execution/blaze/shuffle/uniffle/BlazeUniffleShuffleWriter.scala
+++ spark-extension-shims-spark3/src/main/scala/org/apache/spark/sql/execution/blaze/shuffle/uniffle/BlazeUniffleShuffleWriter.scala
@@ -1,25 +1,34 @@
+/*
+ * Copyright 2022 The Blaze Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.spark.sql.execution.blaze.shuffle.uniffle
 
-import org.apache.spark.{SparkConf, TaskContext}
-import org.apache.spark.executor.ShuffleWriteMetrics
-import org.apache.spark.shuffle.{RssShuffleHandle, RssShuffleManager, ShuffleHandleInfo, ShuffleWriteMetricsReporter}
 import org.apache.spark.shuffle.writer.RssShuffleWriter
-import org.apache.spark.sql.execution.blaze.shuffle.BlazeRssShuffleWriterBase
-import org.apache.uniffle.client.api.ShuffleWriteClient
+import org.apache.spark.shuffle.{ShuffleHandle, ShuffleWriteMetricsReporter}
+import org.apache.spark.sql.execution.blaze.shuffle.{BlazeRssShuffleWriterBase, RssPartitionWriterBase}
 
-class BlazeUniffleShuffleWriter[K, V, C](appId: String,
-                                         shuffleId: Int,
-                                         taskId: String,
-                                         taskAttemptId: Long,
-                                         metrics: ShuffleWriteMetrics,
-                                         shuffleManager: RssShuffleManager,
-                                         sparkConf: SparkConf,
-                                         shuffleWriteClient: ShuffleWriteClient,
-                                         handle: RssShuffleHandle[K, V, C],
-                                         taskFailureCallback: java.util.function.Function[String, Boolean],
-                                         shuffleHandleInfo: ShuffleHandleInfo,
-                                         context: TaskContext)
-  extends RssShuffleWriter[K, V, C](appId, shuffleId, taskId, taskAttemptId, metrics, shuffleManager, sparkConf, shuffleWriteClient, handle, taskFailureCallback, context)
-  with BlazeRssShuffleWriterBase[K, C](metrics) {
+class BlazeUniffleShuffleWriter[K, V, C](
+    rssShuffleWriter: RssShuffleWriter[K, V, C],
+    metrics: ShuffleWriteMetricsReporter)
+    extends BlazeRssShuffleWriterBase[K, V](metrics) {
 
+  override def getRssPartitionWriter(
+      handle: ShuffleHandle,
+      mapId: Int,
+      metrics: ShuffleWriteMetricsReporter,
+      numPartitions: Int): RssPartitionWriterBase = ???
+
+  override def getPartitionLengths(): Array[Long] = ???
 }
